2c4de2f955f6fe6f64469a264ca845e291cba153
MrSn <105975100+sshukurzade@users.noreply.github.com>
2024-04-25 14:27:58 +0400

    Create shukurzade.js

---
+++ shukurzade.js
@@ -0,0 +1,42 @@
+exports.toEmoji = function (input) {
+if(!input) return;
+if(input.length > 50) return;
+const list = {
+    ' ': '   ',
+    '0': ':zero:',
+    '1': ':one:',
+    '2': ':two:',
+    '3': ':three:',
+    '4': ':four:',
+    '5': ':five:',
+    'İ': ':regional_indicator_i:',
+    'Ö': ':regional_indicator_o:',
+    'ö': ':regional_indicator_o:',
+    'Ş': ':regional_indicator_s:',
+    'Ü': ':regional_indicator_u:',
+    'Ç': ':regional_indicator_c:', 
+    'ı': ':regional_indicator_i:', 
+    'o': ':regional_indicator_o:',
+    'ş': ':regional_indicator_s:',
+    'ğ': ':regional_indicator_g:',
+    'Ğ': ':regional_indicator_g:',
+    'ü': ':regional_indicator_u:',
+    'ç': ':regional_indicator_c:', 
+    '6': ':six:',
+    '7': ':seven:',
+    '8': ':eight:',
+    '9': ':nine:',
+    '!': ':grey_exclamation:',
+    '?': ':grey_question:',
+    '#': ':hash:',
+    '*': ':asterisk:'
+    };
+    
+    'abcdefghijklmnopqrstuvwxyz'.split('').forEach(n => {
+    list[n] = list[n.toUpperCase()] = ` :regional_indicator_${n}:`;
+    });
+
+const result = input.split('').map(n => list[n] || n).join('')
+
+	return `${result}`;
+};
